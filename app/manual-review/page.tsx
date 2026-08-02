314b4ee46a66a80dcb95ea919317c8ac7f506eb2
Xinyu Zhang <92386844+XinyuIvy@users.noreply.github.com>
2026-08-02 15:24:54 -0400

    Remove separate manual review page

--- app/manual-review/page.tsx
+++
@@ -1,7 +0,0 @@
-import ManualReviewClient from "./manual-review-client";
-
-export const dynamic = "force-dynamic";
-
-export default function ManualReviewPage() {
-  return <ManualReviewClient />;
-}
